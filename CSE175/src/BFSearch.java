be5f4d0db8282c464417b677dacba905c929df3e
ZoREfLy <zoreflyc@gmail.com>
2017-09-24 21:01:16 -0700

    implementing BFS and DFS

--- CSE175/src/BFSearch.java
+++ CSE175/src/BFSearch.java
@@ -1,7 +1,14 @@
 public class BFSearch {
 
+    public int expansionCount;
+
     // constructor
     public BFSearch(){
+
+    }
+
+    //search function
+    public Waypoint search(boolean true_or_false){
 
     }
 }
--- CSE175/src/DFSearch.java
+++ CSE175/src/DFSearch.java
@@ -1,7 +1,14 @@
 public class DFSearch {
 
+    public int expansionCount;
+
     //constructor
     public DFSearch(){
+
+    }
+
+    //search function
+    public Waypoint search(boolean true_or_false){
 
     }
 }
