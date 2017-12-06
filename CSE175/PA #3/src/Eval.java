40caa4248b2f1e8ab5dc44c7ee34dacdbde86ff0
ZoREfLy <zoreflyc@gmail.com>
2017-12-05 23:14:44 -0800

    update

--- CSE175/PA #3/src/Eval.java
+++ CSE175/PA #3/src/Eval.java
@@ -17,6 +17,16 @@
 // David Noelle -- Sat Nov  3 16:37:10 PDT 2012
 //
 
+// Header Comment:
+// In this Programming Assignment, we only implement function value_roll_hand and 
+// function heuristic. For value_roll_hand, we find out all possible outcomes of 
+// dice roll and calculate utility values for those outcome. We can call value_rolled_hand
+// to do the actual calculation. Heuristic function is used to calculate the quality of 
+// current state for computer and it return a real number to grade the quality. If the
+// number returned is positive, the current state is good for computer, whereas, user is in
+// good place. 
+
+import com.sun.xml.internal.bind.v2.TODO;
 
 public class Eval {
 
@@ -29,23 +39,23 @@ public class Eval {
     //          non-terminal states, backing up heuristic evaluation 
     //          values once the given depth has reached the depth limit.
     static public double value (State s, int depth) {
-		// Stop searching once either a terminal state is reached or the
-		// depth limit is reached ...
-		if ((s.terminal()) || (depth >= depth_limit)) {
-	    	return (s.payoff());
-		}
-		// Keep searching ...
-		switch (s.current_choice) {
-		case roll:
-	    	return (Eval.value_roll(s, depth));
-		case stop:
-	    	return (Eval.value_stop(s, depth));
-		case undecided:
-	    	return (Eval.value_choose(s, depth));
-		default:
-	    	// We should never get here ...
-	    	return (0.0);
-		}
+	// Stop searching once either a terminal state is reached or the
+	// depth limit is reached ...
+	if ((s.terminal()) || (depth >= depth_limit)) {
+	    return (s.payoff());
+	}
+	// Keep searching ...
+	switch (s.current_choice) {
+	case roll:
+	    return (Eval.value_roll(s, depth));
+	case stop:
+	    return (Eval.value_stop(s, depth));
+	case undecided:
+	    return (Eval.value_choose(s, depth));
+	default:
+	    // We should never get here ...
+	    return (0.0);
+	}
     }
 
     // value -- This public function returns the payoff value of 
@@ -60,145 +70,151 @@ public class Eval {
     //                      state, given that the hand has just been 
     //                      rolled to the specified dice faces.
     static double value_rolled_hand (State rolled_s, int depth) {
-		State s = new State(rolled_s);
-		double val = 0.0;                // return value
+	State s = new State(rolled_s);
+	double val = 0.0;                // return value
 
-		// Collect brain and blast dice from the hand ...
-		s.collectHand();
-		// Check to see if the current player has been shotgunned ...
-		if (s.shotgunned()) {
-	    	// This turn is over, so force the choice to stop ...
-	    	s.current_choice = Choice.stop;
-	    	// Calculate the expected utility value of the resulting
-	    	// state by processing the "stop" action ...
-	    	val = Eval.value(s, depth);
-		} else {
-	    	// The roll is done, but the turn is not, so set the
-	    	// choice to undecided ...
-	    	s.current_choice = Choice.undecided;
-	    	// Calculate the expected utility value of the resulting
-	    	// state.  Note that this is one of the two places where
-	    	// the "depth" is incremented ...
-	    	val = Eval.value(s, (depth + 1));
-		}
-		// Deallocate storage ...
-		s = null;
-		// Return the expected value ...
-		return (val);
+	// Collect brain and blast dice from the hand ...
+	s.collectHand();
+	// Check to see if the current player has been shotgunned ...
+	if (s.shotgunned()) {
+	    // This turn is over, so force the choice to stop ...
+	    s.current_choice = Choice.stop;
+	    // Calculate the expected utility value of the resulting
+	    // state by processing the "stop" action ...
+	    val = Eval.value(s, depth);
+	} else {
+	    // The roll is done, but the turn is not, so set the 
+	    // choice to undecided ...
+	    s.current_choice = Choice.undecided;
+	    // Calculate the expected utility value of the resulting 
+	    // state.  Note that this is one of the two places where 
+	    // the "depth" is incremented ...
+	    val = Eval.value(s, (depth + 1));
+	}
+	// Deallocate storage ...
+	s = null;
+	// Return the expected value ...
+	return (val);
     }
 
     // value_roll_hand -- Compute the expected utility value of this 
     //                    state, given that the hand is full.  Note that
     //                    this function assumes that there are three dice
-    //                    in a hand (i.e., that the value of "have_size"
+    //                    in a hand (i.e., that the value of "have_size" 
     //                    is three).
     static double value_roll_hand (State s, int depth) {
-		double val = 0.0;           // return value
+	double val = 0.0;           // return value
 
-		// PLACE YOUR CODE HERE!
-		//
-		// YOUR CODE SHOULD CALCULATE THE EXPECTED UTILITY VALUE OF
-		// THE GIVEN STATE, GIVEN THAT THE THREE DICE CURRENTLY IN
-		// THE HAND WILL IMMEDIATELY BE ROLLED BY THE CURRENT PLAYER.
-		//
-		// YOUR CODE SHOULD CALL "value_rolled_hand" AT SOME POINT.
+	// PLACE YOUR CODE HERE!
+	// 
+	// YOUR CODE SHOULD CALCULATE THE EXPECTED UTILITY VALUE OF
+	// THE GIVEN STATE, GIVEN THAT THE THREE DICE CURRENTLY IN
+	// THE HAND WILL IMMEDIATELY BE ROLLED BY THE CURRENT PLAYER.
+	// 
+	// YOUR CODE SHOULD CALL "value_rolled_hand" AT SOME POINT.
+	
+	// Following three for-loops are used to generate all possible
+	// outcomes for three dice. DieFace will become invalid, but we don't want
+	// encounter such situation. Thus, when die is invalid, we use
+	// continue to skip it.
+	for (DieFace die1 : DieFace.values()) { // choose a symbol for first die
+		// don't let the first die become invalid
+		if (die1 == DieFace.invalid) {
+			continue; 
+		} else {
+			for (DieFace die2 : DieFace.values()) { // choose a symbol for the second die
+				// don't let the second die become invalid
+				if (die2 == DieFace.invalid) {
+					continue;
+				} else {
+					for (DieFace die3 : DieFace.values()) { // choose a symbol for the third die
+						// don't let the third die become invalid
+						if (die3 == DieFace.invalid) {
+							continue;
+						} else {
+							// calculate the probability of value of current symbol value for three dice
+							double prob = s.rollProb(die1, die2, die3);
+							// roll the three dice
+							s.roll(die1, die2, die3);
+							// calculate the value for rolled dice
+							val += prob * value_rolled_hand(s, depth);
+						}
+					}
+				}
+			}
+		}
+	}
 
-        // calculate the utility value for every single situation
-        for (DieFace x : DieFace.values())  {
-            // calculate the utility value when each die has a particular symbol
-            if (x ==  DieFace.invalid) {
-                continue; // we want know the particular symbol
-            } else {
-                for (DieFace y : DieFace.values()) {
-                    if (y  ==  DieFace.invalid) {
-                        continue;
-                    } else {
-                        for (DieFace z : DieFace.values()) {
-                            if (z == DieFace.invalid) {
-                                continue;
-                            } else {
-                                // calculate the probability for current situation (each die has a symbol)
-                                double prob = s.rollProb(x, y, z);
-                                s.roll(x, y, z);
-                                // calculate the utility value
-                                val += prob * value_rolled_hand(s, depth);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-		return (val);
+	return (val);
     }
 
     // value_roll -- Compute the expected utility value of this state, 
     //               given that the current player will be immediately 
     //               drawing dice and rolling.
     static double value_roll (State s, int depth) {
-		double val = 0.0;           // return value
+	double val = 0.0;           // return value
 
-		if (s.numDiceInHand() == State.hand_size) {
-			// No need to draw more dice, so we need to consider all
-	        // possible results of rolling the dice in hand ...
-	        val = Eval.value_roll_hand(s, depth);
+	if (s.numDiceInHand() == State.hand_size) {
+	    // No need to draw more dice, so we need to consider all
+	    // possible results of rolling the dice in hand ...
+	    val = Eval.value_roll_hand(s, depth);
+	} else {
+	    // Need to draw a die ...
+	    if (s.cupIsEmpty()) {
+		// The cup is empty.  According to the official rules,
+		// we should reuse collected brain dice at this point ...
+		State refilled_state = new State(s);
+		refilled_state.reuseBrains();
+		val = Eval.value_roll(refilled_state, depth);
+		refilled_state = null;
 	    } else {
-	        // Need to draw a die ...
-	        if (s.cupIsEmpty()) {
-		    // The cup is empty.  According to the official rules,
-		    // we should reuse collected brain dice at this point ...
-		    State refilled_state = new State(s);
-		    refilled_state.reuseBrains();
-		    val = Eval.value_roll(refilled_state, depth);
-		    refilled_state = null;
-	        } else {
-		        // Iterate over all possible colors for the next die ...
-		        for (DieColor c : DieColor.values()) {
-		            if (c != DieColor.invalid) {
-			            double this_draw_prob = s.drawProb(c);
-			            // Draw die of this color ...
-			            Die d = s.draw(c);
-			            if (d != null) {
-			                // Recursive call ...
-			                double draw_val = Eval.value_roll(s, depth);
-			                // Update the expected utility value over all
-			                // colors for this die ...
-			                val = val + (draw_val * this_draw_prob);
-			                // Replace the drawn die in the cup ...
-			                s.replace(d);
-			            }
-		            }
-		        }
-	        }
+		// Iterate over all possible colors for the next die ...
+		for (DieColor c : DieColor.values()) {
+		    if (c != DieColor.invalid) {
+			double this_draw_prob = s.drawProb(c);
+			// Draw die of this color ...
+			Die d = s.draw(c);
+			if (d != null) {
+			    // Recursive call ...
+			    double draw_val = Eval.value_roll(s, depth);
+			    // Update the expected utility value over all 
+			    // colors for this die ...
+			    val = val + (draw_val * this_draw_prob);
+			    // Replace the drawn die in the cup ...
+			    s.replace(d);
+			}
+		    }
+		}
 	    }
-	    // Return expected value ...
-	    return (val);
+	}
+	// Return expected value ...
+	return (val);
     }
 
     // value_stop -- Compute the expected utility value of this state, 
     //               given that the current player will not continue 
     //               to roll at this point.
     static double value_stop (State stop_s, int depth) {
-	    State s = new State(stop_s);
-	    double val = 0.0;                // return value
+	State s = new State(stop_s);
+	double val = 0.0;                // return value
 
-	    // Update scores ...
-	    s.endTurn();
-	    // Check for end of game ...
-	    if (s.terminal()) {
-	        val = s.payoff();
-	    } else {
-	        // Move to next player ...
-	        s.nextPlayer();
-	        // Recursively calculate the expected utility value of the
-	        // next player's choice node.  Note that this is one of the
-	        // two places where "depth" is incremented.
-	        val = Eval.value(s, (depth + 1));
-	    }
-	    // Deallocate storage ...
-	    s = null;
-	    // Return value ...
-	    return (val);
+	// Update scores ...
+	s.endTurn();
+	// Check for end of game ...
+	if (s.terminal()) {
+	    val = s.payoff();
+	} else {
+	    // Move to next player ...
+	    s.nextPlayer();
+	    // Recursively calculate the expected utility value of the
+	    // next player's choice node.  Note that this is one of the 
+	    // two places where "depth" is incremented.
+	    val = Eval.value(s, (depth + 1));
+	}
+	// Deallocate storage ...
+	s = null;
+	// Return value ...
+	return (val);
     }
 
     // value_choose -- Compute the expected utility value of each of two 
@@ -207,42 +223,42 @@ public class Eval {
     //                 player, and return the lesser of these two values 
     //                 if the user is the current player.
     static double value_choose (State s, int depth) {
-	    double eu_roll;  // expected utility value of rolling
-	    double eu_stop;  // expected utility value of stoping
+	double eu_roll;  // expected utility value of rolling
+	double eu_stop;  // expected utility value of stoping
 
-	    // Always roll if no brains have been collected ...
-	    if (s.brains_collected == 0) {
-	        s.current_choice = Choice.roll;
-	        eu_roll = Eval.value(s, depth);
-	        // Revert the state ...
-	        s.current_choice = Choice.undecided;
-	        // Return value of rolling ...
-	        return (eu_roll);
-	    }
-	    // First, calculate the case of choosing to roll ...
+	// Always roll if no brains have been collected ...
+	if (s.brains_collected == 0) {
 	    s.current_choice = Choice.roll;
 	    eu_roll = Eval.value(s, depth);
-	    // Now, calculate the case of choosing to stop ...
-	    s.current_choice = Choice.stop;
-	    eu_stop = Eval.value(s, depth);
 	    // Revert the state ...
 	    s.current_choice = Choice.undecided;
-	    // Which one is better depends on whose turn it is ...
-	    if (s.current_player == Turn.computer) {
-	        // MAX node -- Looking for high values ...
-	        if (eu_roll >= eu_stop) {
-		        return (eu_roll);
-	        } else {
-		    return (eu_stop);
-	        }
+	    // Return value of rolling ...
+	    return (eu_roll);
+	}
+	// First, calculate the case of choosing to roll ...
+	s.current_choice = Choice.roll;
+	eu_roll = Eval.value(s, depth);
+	// Now, calculate the case of choosing to stop ...
+	s.current_choice = Choice.stop;
+	eu_stop = Eval.value(s, depth);
+	// Revert the state ...
+	s.current_choice = Choice.undecided;
+	// Which one is better depends on whose turn it is ...
+	if (s.current_player == Turn.computer) {
+	    // MAX node -- Looking for high values ...
+	    if (eu_roll >= eu_stop) {
+		return (eu_roll);
 	    } else {
-	        // MIN node -- Looking for low values ...
-	        if (eu_roll <= eu_stop) {
-		        return (eu_roll);
-	        } else {
-		        return (eu_stop);
-	        }
+		return (eu_stop);
 	    }
+	} else {
+	    // MIN node -- Looking for low values ...
+	    if (eu_roll <= eu_stop) {
+		return (eu_roll);
+	    } else {
+		return (eu_stop);
+	    }
+	}
     }
 
     // heuristic -- Compute a heuristic evaluation function value for the
@@ -252,20 +268,30 @@ public class Eval {
     //              "State.win_payoff".  The heuristic evaluation value
     //              is returned.
     static public double heuristic (State s) {
-	    // Heuristic value to be returned ...
-	    double value = 0.0;
+		// Heuristic value to be returned ...
+		double value = 0.0;
 
-	    // PLACE YOUR CODE HERE!
-	    //
-	    // YOUR CODE SHOULD ESTIMATE THE EXPECTED UTILITY VALUE OF
-	    // THE GIVEN STATE WITHOUT PERFORMING ANY LOOK-AHEAD SEARCH.
-	    // THE RETURNED HEURISTIC VALUE SHOULD BE BETWEEN PLUS AND
-	    // MINUS "State.win_payoff".  THIS FUNCTION SHOULD BE FAST.
+		// PLACE YOUR CODE HERE!
+		//
+		// YOUR CODE SHOULD ESTIMATE THE EXPECTED UTILITY VALUE OF
+		// THE GIVEN STATE WITHOUT PERFORMING ANY LOOK-AHEAD SEARCH.
+		// THE RETURNED HEURISTIC VALUE SHOULD BE BETWEEN PLUS AND
+		// MINUS "State.win_payoff".  THIS FUNCTION SHOULD BE FAST.
 
+		// calculate the difference between # of brains that computer has and user_brains_eaten
+		// if diff_comp_user is negative, that means user has more brains
+		// if diff_comp_user is positive, that means computer has more brains
+		double diff_comp_user = s.comp_brains_eaten + s.brains_collected - s.user_brains_eaten;
 
-	    // Return the resulting heuristic value ...
-	    return (value);
-    }
+		// The one who first get 13 brains wins the game. Divide diff_comp_user to 13,
+		// we get how far is the game. Times the win_payoff, we get the heuristic value
+		// finally we get the quality of current state
+		value = (diff_comp_user / s.brains_to_win) * s.win_payoff;
+
+		// Return the resulting heuristic value ...
+		return (value);
+	}
+
 
 }
 
